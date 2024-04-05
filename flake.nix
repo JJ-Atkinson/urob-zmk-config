599c3f5c4999aeac3251e552d252d63792d4f861
jarrett <jarrett@freeformsoftware.dev>
2024-04-04 19:25:03 -0500

    Add cradio and build items

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A clj-nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+
+      in {
+        formatter = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git
+            pkgs.podman
+          ];
+        };
+
+      });
+}
